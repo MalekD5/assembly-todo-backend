a5db870b1629ae4b592bdfb58fa51a94c1ab5b90
Malek <106095300+MalekD5@users.noreply.github.com>
2025-06-06 22:11:48 +0300

    feat: add send_failed_msg

--- src/errors.asm
+++ src/errors.asm
@@ -9,6 +9,7 @@ section .data
     socket_fail_msg db "Socket creation failed", 0
     bind_fail_msg db "Bind failed", 0
     listen_fail_msg db "Listen failed", 0
+    send_failed_msg db "Unable to send response", 0
     response_404 db "HTTP/1.1 404 Not Found",13,10
                  db "Content-Type: text/html; charset=UTF-8",13,10
                  db "Content-Length: 75",13,10
@@ -21,21 +22,25 @@ global fail_socket
 global fail_bind
 global fail_listen
 global fail_404
+global fail_send_message
 
 fail_socket:
     lea rcx, [rel format_str]
     lea rdx, [rel socket_fail_msg]
     call printf
+    ret
 
 fail_bind:
     lea rcx, [rel format_str]
     lea rdx, [rel bind_fail_msg]
     call printf
+    ret 
 
 fail_listen:
     lea rcx, [rel format_str]
     lea rdx, [rel listen_fail_msg]
     call printf
+    ret 
 
 fail_404:
     mov rcx, [rel client_socket]
@@ -43,3 +48,9 @@ fail_404:
     mov r8d, response_404_len
     xor r9d, r9d
     call send
+    ret
+
+fail_send_message:
+    lea rcx, [rel format_str]
+    lea rdx, [rel send_failed_msg]
+    call printf
