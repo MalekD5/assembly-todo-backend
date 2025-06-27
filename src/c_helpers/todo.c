382886b3b9ccfdae901046db7dc6c283b04131f1
Malek <106095300+MalekD5@users.noreply.github.com>
2025-06-27 16:12:33 +0300

    feat: improved todo handling

--- src/c_helpers/todo.c
+++ src/c_helpers/todo.c
@@ -1,49 +1,68 @@
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-#define MAX_TODOS 64
-#define MAX_LEN 128
+typedef struct
+{
+    char **items;
+    int count;
+    int capacity;
+} TodoList;
 
-char todo_list[MAX_TODOS][MAX_LEN];
-int todo_count = 0;
+static TodoList todo_list;
+
+void todo_init()
+{
+    todo_list.capacity = 8;
+    todo_list.count = 0;
+    todo_list.items = malloc(sizeof(char *) * todo_list.capacity);
+}
+
+void todo_free()
+{
+    for (int i = 0; i < todo_list.count; ++i)
+        free(todo_list.items[i]);
+    free(todo_list.items);
+    todo_list.items = NULL;
+    todo_list.count = todo_list.capacity = 0;
+}
 
 void todo_add(const char *text)
 {
-    if (todo_count < MAX_TODOS)
+    if (todo_list.count >= todo_list.capacity)
     {
-        strncpy(todo_list[todo_count], text, MAX_LEN - 1);
-        todo_list[todo_count][MAX_LEN - 1] = 0; // null-terminate
-        todo_count++;
+        todo_list.capacity *= 2;
+        todo_list.items = realloc(todo_list.items, sizeof(char *) * todo_list.capacity);
     }
+    todo_list.items[todo_list.count++] = strdup(text);
 }
 
 const char *todo_get(int index)
 {
-    if (index >= 0 && index < todo_count)
-    {
-        return todo_list[index];
-    }
-    return 0;
+    return (index >= 0 && index < todo_list.count) ? todo_list.items[index] : NULL;
 }
 
 void todo_delete(int index)
 {
-    if (index >= 0 && index < todo_count)
+    if (index >= 0 && index < todo_list.count)
     {
-        memmove(todo_list[index], todo_list[index + 1], MAX_LEN);
-        todo_count--;
+        free(todo_list.items[index]);
+        for (int i = index; i < todo_list.count - 1; ++i)
+            todo_list.items[i] = todo_list.items[i + 1];
+        todo_list.count--;
     }
 }
 
 void todo_update(int index, const char *text)
 {
-    if (index >= 0 && index < todo_count)
+    if (index >= 0 && index < todo_list.count)
     {
-        strncpy(todo_list[index], text, MAX_LEN - 1);
-        todo_list[index][MAX_LEN - 1] = 0; // null-terminate
+        free(todo_list.items[index]);
+        todo_list.items[index] = strdup(text);
     }
 }
 
 int todo_total()
 {
-    return todo_count;
+    return todo_list.count;
 }
