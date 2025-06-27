fac68fbfbbcca0cc1e5d348987d5850148f4cb8a
Malek <106095300+MalekD5@users.noreply.github.com>
2025-06-27 18:33:51 +0300

    feat: complete post todo route to add todos correctly

--- src/routes/post_todo.asm
+++ src/routes/post_todo.asm
@@ -7,16 +7,19 @@ extern sprintf, printf, strlen, strcat
 
 extern GetProcessHeap, HeapAlloc, HeapFree
 
+extern cJSON_GetObjectItemCaseSensitive, cJSON_GetStringValue, cJSON_Print
+
 section .data
-    post_response_fmt db "HTTP/1.1 201 Created",13,10
-                       db "Content-Type: application/json",13,10
-                       db "Content-Length: %d",13,10
+    post_response_fmt  db "HTTP/1.1 201 Created",13,10
+                       db "Content-Type: text/plain",13,10
+                       db "Content-Length: 0",13,10
                        db "Connection: close",13,10,13,10,0
-    json_response_fmt  db '{ "todo": %s }',0
-    debug_msg db "Sending count response",10,0
     heap_fail_msg db "Failed to allocate heap memory", 10, 0
-    debug_json_fmt db "Parsed JSON body: %s", 10, 0
     msg_parse_fail db "JSON parse failed", 10, 0
+    ok_response db "OK", 10, 0
+
+section .rodata
+    key_name db "todo", 0
 
 section .text
 global post_todo
@@ -32,73 +35,47 @@ post_todo:
     test rax, rax
     jz .bad_json
 
-    lea rsi, [r15 + connection.recv_buffer]
-    mov rax, [r15 + connection.body_offset]
-    add rsi, rax
-    lea rcx, [rel debug_json_fmt]
-    mov rdx, rsi
+    mov rcx, [r15 + connection.json_root]    ; rcx = cJSON *
+
+    call cJSON_Print                ; rax = const char* to string
+
+    lea rcx, [rel rax]
     call printf
 
+    call GetProcessHeap
 
-    ; call GetProcessHeap
-
-    ; mov rcx, rax
-    ; xor rdx, rdx
-    ; mov r8, response_struc_size
-    ; call HeapAlloc
+    mov rcx, rax
+    xor rdx, rdx
+    mov r8, response_struc_size
+    call HeapAlloc
     
-    ; test rax, rax
-    ; jz .fail_heap_alloc
+    test rax, rax
+    jz .fail_heap_alloc
 
-    ; mov r15, rax ; store the pointer to the response struct in r15
+    mov rcx, [r15 + connection.json_root]
+    lea rdx, [rel key_name]
+    call cJSON_GetObjectItemCaseSensitive
 
-    ; call todo_add
-    ; mov ecx, eax
-    ; mov edx, eax
+    mov rcx, rax
+    call cJSON_GetStringValue
 
-    ; lea rcx, [r15 + response_struc.json_response_buffer]
-    ; lea rdx, [rel json_response_fmt]
-    ; mov r8d, eax
-    ; xor r9d, r9d
-    ; call sprintf
+    mov rcx, rax
+    call todo_add
+    mov ecx, eax
+    mov edx, eax
 
-    ; lea rcx, [r15 + response_struc.json_response_buffer]
-    ; call strlen
+    mov rcx, r14 ; connection
+    lea rsi, [rel post_response_fmt]
+    xor r9d, r9d
+    call send_response
 
-    ; lea rcx, [r15 + response_struc.response_buffer]
-    ; lea rdx, [rel post_response_fmt]
-    ; mov r8d, eax
-    ; call sprintf
+    call GetProcessHeap
+    mov rbx, rax
 
-    ; lea rcx, [r15 + response_struc.response_buffer]
-    ; lea rdx, [r15 + response_struc.json_response_buffer]
-    ; call strcat
-
-    ; lea rcx, [rel debug_msg]
-    ; call printf
-
-    ; lea rsi, [r15 + response_struc.response_buffer]
-    ; xor ecx, ecx
-
-    ; lea rcx, [r15 + response_struc.response_buffer]
-    ; call printf
-
-    ; lea rcx, [r15 + response_struc.response_buffer]
-    ; call strlen
-    ; mov r8d, eax
-
-    ; mov rcx, r14 ; connection
-    ; lea rdx, [r15 + response_struc.response_buffer]
-    ; xor r9d, r9d
-    ; call send_response
-
-    ; call GetProcessHeap
-    ; mov rbx, rax
-
-    ; mov rcx, rbx
-    ; mov rdx, 0
-    ; mov r8, r15
-    ; call HeapFree
+    mov rcx, rbx
+    mov rdx, 0
+    mov r8, r15
+    call HeapFree
 
     add rsp, 40
     pop r15
